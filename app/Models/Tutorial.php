1541de98696fd8e2f58b778acfa88af85ef9a682
Ikennaf1 <iiikbros202@gmail.com>
2024-11-15 13:13:33 +0100

    Scaffolding tutorials

--- app/Models/Tutorial.php
+++ app/Models/Tutorial.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tutorial extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'user_id',
+        'featured_image',
+        'description',
+    ];
+
+    /**
+     * The users that belong to the role.
+     */
+    public function users(): BelongsTo
+    {
+        return $this->BelongsTo(User::class);
+    }
 }
